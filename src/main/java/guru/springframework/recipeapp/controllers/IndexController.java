a6018e3c28026feeea35bf5d27dbaffbb912b866
ed <ed@somecompany.com>
2020-01-26 14:16:47 -0500

    added code to retrieve data from Category and UnitOfMeasure repositories

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -5,14 +5,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.recipeapp.domain.Category;
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import guru.springframework.recipeapp.repositories.CategoryRepository;
+import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+
 @Controller
 public class IndexController {
+	
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @RequestMapping(value= {"", "/", "index"}, method=RequestMethod.GET)
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+    
+	@RequestMapping(value= {"", "/", "index"}, method=RequestMethod.GET)
     public String getIndexPage(Model model) {
-//        model.addAttribute(attributeName, attributeValue);
-        System.out.println("Some message to say... 1234");
-        return "index";
+		Category category = categoryRepository.findByDescription("American").get();
+		UnitOfMeasure unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon").get();
+
+		System.out.println("Category id: " + category.getId());
+		System.out.println("Uom id: " + unitOfMeasure.getId());
+		
+    	return "index";
     }
     
 }
